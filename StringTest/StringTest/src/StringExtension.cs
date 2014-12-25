bb241407468eb712429b010b54c932b6f2c688e1
Subhrajit Roy <roy.subhrajit@gmail.com>
2014-12-25 18:53:45 +0000

    Add a test c# project to build artifact

---
+++ StringTest/StringTest/src/StringExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace StringTest
+{
+    public static class StringExtension
+    {
+        public static string Underscore(this string value)
+        {
+            return string.Format("_{0}", value);
+        }
+    }
+}
---
+++ StringTest/StringTest/test/StringTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringTest.test
+{
+    [TestClass]
+    public class StringTest
+    {
+        [TestMethod]
+        public void ShouldAddUnderscoreToString()
+        {
+            Assert.AreEqual("_test","test".Underscore());
+        }
+    }
+}
